5e2e16a7588ed1c2773afe1126458db979d11292
zayan ahmed <zayank4774@gmail.com>
2026-09-13 19:27:18 +0500

    feat: package Windows desktop capture agent

--- desktop-agent/preload.js
+++ desktop-agent/preload.js
@@ -2,5 +2,8 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("captureAgent", {
   getSources: () => ipcRenderer.invoke("capture:sources"),
-  uploadRecording: ({ blob, title, durationSeconds, appUrl }) => blob.arrayBuffer().then((buffer) => ipcRenderer.invoke("recording:upload", { buffer, title, durationSeconds, appUrl }))
+  getEvents: ({ appUrl, token }) => ipcRenderer.invoke("agent:events", { appUrl, token }),
+  openExternal: (url) => ipcRenderer.invoke("open:external", url),
+  onCalendarEvent: (callback) => ipcRenderer.on("calendar:event", (_event, data) => callback(data)),
+  uploadRecording: ({ blob, title, durationSeconds, appUrl, token, calendarEventId }) => blob.arrayBuffer().then((buffer) => ipcRenderer.invoke("recording:upload", { buffer, title, durationSeconds, appUrl, token, calendarEventId }))
 });
